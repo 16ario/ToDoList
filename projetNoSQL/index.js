af1b9fa3d1472b78845663e58cb6d7186b6b4cb5
Césario Bailleul <cesariobailleul16@gmail.com>
2025-06-05 10:42:25 +0200

    suppression subtask

--- projetNoSQL/index.js
+++ projetNoSQL/index.js
@@ -17,24 +17,22 @@ mongoose.connect(dburl, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
+
 app.get('/', async (req, res) => {
     try {
-        const todos = await Todo.find(); // subtasks sont dans le tableau déjà
+        const todos = await Todo.find();
         res.render('index', { data: todos });
     } catch (err) {
         res.status(500).send('Erreur serveur');
     }
 });
 
-
 app.post('/', (req, res) => {
     const { todoValue } = req.body;
-
     const todo = new Todo({
         todo: todoValue,
         subtasks: []
     });
-
     todo.save()
         .then(() => res.redirect('/'))
         .catch(err => {
@@ -45,11 +43,10 @@ app.post('/', (req, res) => {
 
 app.post('/:id/subtask', async (req, res) => {
     const { subtaskTitle } = req.body;
-
     try {
         await Todo.findByIdAndUpdate(
             req.params.id,
-            { $push: { subtasks: { title: subtaskTitle } } }, // pas juste un string
+            { $push: { subtasks: { title: subtaskTitle } } },
             { new: true }
         );
         res.redirect('/');
@@ -59,7 +56,6 @@ app.post('/:id/subtask', async (req, res) => {
     }
 });
 
-
 app.delete('/:id', (req, res) => {
     Todo.findByIdAndDelete(req.params.id)
         .then(() => res.status(200).send("Todo deleted successfully"))
@@ -67,6 +63,20 @@ app.delete('/:id', (req, res) => {
             console.error(err);
             res.status(500).send("Internal Server Error");
         });
+});
+
+app.delete('/:todoId/subtask/:subtaskId', async (req, res) => {
+    const { todoId, subtaskId } = req.params;
+    try {
+        await Todo.findByIdAndUpdate(
+            todoId,
+            { $pull: { subtasks: { _id: subtaskId } } }
+        );
+        res.status(200).send('Sous-tâche supprimée');
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Erreur lors de la suppression de la sous-tâche');
+    }
 });
 
 app.listen(port, () => {
