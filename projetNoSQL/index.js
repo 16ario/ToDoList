ba3be9b974fc9554be54d4a9d4c85e6cb333eb27
Césario Bailleul <cesariobailleul16@gmail.com>
2025-06-05 11:16:22 +0200

    tags

--- projetNoSQL/index.js
+++ projetNoSQL/index.js
@@ -18,21 +18,28 @@ mongoose.connect(dburl, {
     useUnifiedTopology: true
 });
 
+// Route GET
 app.get('/', async (req, res) => {
     try {
         const todos = await Todo.find();
         res.render('index', { data: todos });
     } catch (err) {
+        console.error(err);
         res.status(500).send('Erreur serveur');
     }
 });
 
+// Ajouter todo
 app.post('/', (req, res) => {
-    const { todoValue } = req.body;
+    const { todoValue, tags } = req.body;
+    const tagsArray = tags ? tags.split(',').map(t => t.trim()).filter(t => t) : [];
+
     const todo = new Todo({
         todo: todoValue,
-        subtasks: []
+        subtasks: [],
+        tags: tagsArray
     });
+
     todo.save()
         .then(() => res.redirect('/'))
         .catch(err => {
@@ -41,8 +48,10 @@ app.post('/', (req, res) => {
         });
 });
 
+// Ajouter subtask
 app.post('/:id/subtask', async (req, res) => {
     const { subtaskTitle } = req.body;
+
     try {
         await Todo.findByIdAndUpdate(
             req.params.id,
@@ -56,26 +65,30 @@ app.post('/:id/subtask', async (req, res) => {
     }
 });
 
-app.delete('/:id', (req, res) => {
-    Todo.findByIdAndDelete(req.params.id)
-        .then(() => res.status(200).send("Todo deleted successfully"))
-        .catch(err => {
-            console.error(err);
-            res.status(500).send("Internal Server Error");
-        });
-});
-
-app.delete('/:todoId/subtask/:subtaskId', async (req, res) => {
-    const { todoId, subtaskId } = req.params;
+// Supprimer todo
+app.delete('/:id', async (req, res) => {
+    console.log("DELETE todo: ", req.params.id);
     try {
-        await Todo.findByIdAndUpdate(
-            todoId,
-            { $pull: { subtasks: { _id: subtaskId } } }
-        );
-        res.status(200).send('Sous-tâche supprimée');
+        await Todo.findByIdAndDelete(req.params.id);
+        res.status(200).send("Todo deleted successfully");
     } catch (err) {
         console.error(err);
-        res.status(500).send('Erreur lors de la suppression de la sous-tâche');
+        res.status(500).send("Internal Server Error");
+    }
+});
+
+// Supprimer sous-tâche
+app.delete('/:todoId/subtask/:subtaskId', async (req, res) => {
+    console.log(`DELETE subtask: todoId=${req.params.todoId}, subtaskId=${req.params.subtaskId}`);
+    try {
+        await Todo.findByIdAndUpdate(
+            req.params.todoId,
+            { $pull: { subtasks: { _id: req.params.subtaskId } } }
+        );
+        res.status(200).send("Subtask deleted successfully");
+    } catch (err) {
+        console.error(err);
+        res.status(500).send("Internal Server Error");
     }
 });
 
--- projetNoSQL/models/todo.js
+++ projetNoSQL/models/todo.js
@@ -1,15 +1,23 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const subtaskSchema = new Schema({
-    title: { type: String, required: true },
-    done: { type: Boolean, default: false }
-});
-
 const todoSchema = new Schema({
-    todo: { type: String, required: true },
-    subtasks: [subtaskSchema],
-    createdAt: { type: Date, default: Date.now }
+    todo: {
+        type: String,
+        required: true
+    },
+    subtasks: [{
+        title: String,
+        done: {
+            type: Boolean,
+            default: false
+        }
+    }],
+    tags: [String],
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
 });
 
 const Todo = mongoose.model("Todo", todoSchema);
